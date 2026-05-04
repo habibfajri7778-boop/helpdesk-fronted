a01760a85768df04724e8461a7b637d1b3bd1d3c
habibfajri7778-boop <habibfajri7778@gmail.com>
2026-05-04 14:00:35 +0700

    fix api.js

--- src/api.js
+++ src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://cupbearer-overcast-domain.ngrok-free.dev/api',
+  baseURL: 'https:///cupbearer-overcast-doamin.ngrok-free.dev/api',
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = Bearer ${token};
+  if (token){
+    config.headers.Authorization = `Bearer ${token}`;
   }
-  config.headers['ngrok-skip-browser-warning'] = 'true';
+  config.headers['ngrok-ski-browser-warning'] = 'true';
   return config;
 });
 
 export default API;
-
